d1128405d2321cde91ce1076430d8e194f21cd16
its-asaduzzaman <samrat.asad@gmail.com>
2022-08-11 10:39:32 +0600

    setup database mysql and register view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/register',[RegisterController::class,'index']);
 
 Route::get('/posts', function () {
     return view('posts.index');
