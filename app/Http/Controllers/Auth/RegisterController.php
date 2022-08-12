b8ab9e44c089fc121c0612072c15b8a4c2b9dfa6
its-asaduzzaman <samrat.asad@gmail.com>
2022-08-12 23:55:38 +0600

    user create controller method

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -24,6 +26,14 @@ class RegisterController extends Controller
 
         dd('store');
         //store user
+        User::create(
+            [
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]
+        );
         //sign the user in
         //redirect
 
