acaa047aa443391345e7ef90b7ffc8cfbd166e69
its-asaduzzaman <samrat.asad@gmail.com>
2022-08-24 19:35:03 +0600

    creat post table and store data on that

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('posts.index');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, ['body' => 'required']);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
