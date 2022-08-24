4a0a940d7085ea90b44a02a0324c2f387aff067c
its-asaduzzaman <samrat.asad@gmail.com>
2022-08-24 19:57:04 +0600

    show post data on dashboard

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Auth\Events\Validated;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::get();
+        return view('posts.index', ['posts' => $posts]);
     }
 
     public function store(Request $request)
