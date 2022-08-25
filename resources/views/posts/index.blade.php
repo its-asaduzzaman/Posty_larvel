60ec3721761288645dfc2788ae9921af86ba6677
its-asaduzzaman <samrat.asad@gmail.com>
2022-08-25 23:00:10 +0600

    pagination ui changed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -22,17 +22,16 @@
             </form>
             @if ($posts->count())
                 @foreach ($posts as $post)
-                    <div class=" mb-4">
+                    <div class=" mb-4 bg-slate-100 p-5 rounded-lg" >
                         <a href="" class=" font-bold">{{ $post->user->name }}</a> <span
                             class=" text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
 
-                        <p class=" mb-2">{{ $post->body }}</p>
+                        <p class=" mb-2 ">{{ $post->body }}</p>
 
                     </div>
                 @endforeach
 
-                {{ $posts->links() }}
-                {{-- {{ $items->links('pagination::semantic-ui') }} --}}
+                {{ $posts->links('pagination::semantic-ui') }}
             @else
                 <p>There are no post</p>
             @endif
