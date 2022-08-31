998926a62851b020c9c3ffa116bc1ba0b50dba1c
its-asaduzzaman <samrat.asad@gmail.com>
2022-08-31 19:41:00 +0600

    one user can give only one like

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class PostLikeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function store(Post $post, Request $request)
     {
         $post->likes()->create([
